99996a0e67df6ae1ad9cb156fe7fbd1d0a55250e
Snir Sharristh <snirs@wix.com>
2023-07-02 16:20:42 +0300

    add swagger

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,11 +2,132 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Returns the list of all posts
+ *     responses:
+ *       200:
+ *         description: The list of the posts
+ */
 router.get('/', postController.getPosts);
+
+/**
+ * @swagger
+ * /posts/title:
+ *   get:
+ *     summary: Returns a post by its title
+ *     parameters:
+ *       - in: query
+ *         name: title
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The post
+ *       404:
+ *         description: The post was not found
+ */
 router.get('/title', postController.getPostByTitle);
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: The post was created
+ */
 router.post('/', postController.addPost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Update an existing post
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The post was updated
+ *       404:
+ *         description: The post was not found
+ */
 router.put('/:id', postController.updatePost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   patch:
+ *     summary: Partially update an existing post
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The post was updated
+ *       404:
+ *         description: The post was not found
+ */
 router.patch('/:id', postController.updatePost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Delete a post
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: The post was deleted
+ *       404:
+ *         description: The post was not found
+ */
 router.delete('/:id', postController.deletePost);
 
 module.exports = router;
